736127c23be537520f1fca0aa4f8e10df351ddc0
WesyHub <35263085+WesyHub@users.noreply.github.com>
2022-02-26 15:50:02 +0100

    Update main.rs

--- ratz/src/main.rs
+++ ratz/src/main.rs
@@ -1,29 +1,13 @@
-use std::net::{TcpListener, TcpStream};
-use std::thread;
-use std::io::{Read, Write, Error};
+use std::net::TcpStream;
 
 
-
-fn handle_client(mut stream: TcpStream) -> Result<(), Error> {
-    println!("Connection depuis: {}", stream.peer_addr()?);
-    let mut buf = [0; 516];
-    loop {
-        let bytes_read = stream.read(&mut buf)?;
-        if bytes_read == 0 { return Ok(()) }
-        stream.write(&buf[..bytes_read])?;
-    }
-}
 
 fn main() {
-    let listener = TcpListener::bind("0.0.0.0:5000").expect("Echec de la connection");
-    for stream in listener.incoming() {
-        match stream {
-            Err(e) => { eprintln!("failed: {}", e)}
-            Ok(stream) => {
-                thread::spawn(move || {
-                    handle_client(stream).unwrap_or_else(|error| eprintln!("{:?}", error));
-                });
-            }
-        }
-    }
+
+    
+    let stream = TcpStream::connect("127.0.0.1:8080")
+                           .expect("Couldn't connect to the server...");
+    stream.set_read_timeout(None).expect("set_read_timeout call failed");
+    assert_eq!(stream.read_timeout().unwrap(), None);
+
 }
