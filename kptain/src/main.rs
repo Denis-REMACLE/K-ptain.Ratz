0d4b4b1b8f0274eba469c466479fd98210af901d
Denis-REMACLE <denis.remacle@outlook.fr>
2022-04-13 14:16:18 +0200

    Changed base code

--- kptain/src/main.rs
+++ kptain/src/main.rs
@@ -1,41 +1,284 @@
-use std::net;
-use std::env;
+use rsa::PublicKey;
+use tokio::io::AsyncReadExt;
+use tokio::io::ReadBuf;
+use tokio::macros::support::poll_fn;
+use tokio::net::TcpListener;
+use tokio::io::AsyncWriteExt;
+use tokio::sync::broadcast;
+use tokio::sync::broadcast::Receiver;
+use tokio::sync::broadcast::Sender;
+use std::io;
+use serde::{Deserialize, Serialize};
+use rsa::{RsaPublicKey, RsaPrivateKey, pkcs8::FromPublicKey, pkcs8::ToPublicKey, PaddingScheme};
+use rand::rngs::OsRng;
+use rusqlite::{params, Connection, Result, OpenFlags};
 
-fn listen(socket: &net::UdpSocket, mut buffer: &mut [u8]) -> std::net::SocketAddr {
 
-    let (number_of_bytes, src_addr) = socket.recv_from(&mut buffer).expect("no data received");
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    println!("{:?}", number_of_bytes);
-    println!("{:?}", src_addr);
-
-    number_of_bytes
+    #[test]
+    // SQLITE READ ONLY return error if try opening DB but is not exist, we can test DB exist with that
+    fn test_db_connection() {
+        let conn = Connection::open_with_flags("/tmp/rust_project.db", OpenFlags::SQLITE_OPEN_READ_ONLY);
+        assert!(conn.is_ok());
+    }
 }
 
-fn send(socket: &net::UdpSocket, receiver: &str, msg: &Vec<u8>) -> usize {
 
-    println!("sending data");
-    let result = socket.send_to(msg, receiver).expect("failed to send message");
 
-    result
+// represent a user
+struct User{
+    username: String,
+    stream: tokio::net::TcpStream,
+    _addr: std::net::SocketAddr, 
 }
 
-fn init_host() -> net::UdpSocket {
 
-    println!("initializing host");
-    let socket = net::UdpSocket::bind(("192.168.1.41", 53)).expect("failed to bind host socket");
-
-    socket
+#[derive(Serialize, Deserialize, Debug)]
+struct Message{
+    user_sender: String,
+    user_receiver: String,
+    message_type: String,
+    message_content: String, 
 }
 
-fn main() {
-    let mut buf: Vec<u8> = Vec::with_capacity(100);
-    let socket = init_host();
-    let message = String::from("hello");
-    let msg_bytes = message.into_bytes();
+fn trim_newline(s:&mut String){
+    while s.ends_with('\n') || s.ends_with('\r') || s.ends_with('\u{0}') {
+        s.pop();
+    };
+}
 
-    loop {
-        while listen(&socket, &mut buf) != 0 {
-            println!("boo");
+// add user check function 
+// message type : user_check
+// message type : user_accept
+// message type : user_not_accept
+// if present snd --> user_not_accept
+// if not present snd --> user_accept
+
+// message type : get_from_db
+// message_type : global
+async fn db_process (channel_snd: Sender<String>, mut channel_rcv : Receiver<String>){
+    let conn = Connection::open("/tmp/rust_project.db").unwrap();
+    loop{
+        match channel_rcv.try_recv(){
+            Ok(n) => {
+                let from_json_message: Message = serde_json::from_str(&n).unwrap();
+                if from_json_message.message_type == "global" {
+                    conn.execute("INSERT INTO message (sender, message_type, message_content) VALUES (?1, ?2, ?3)",
+                     params![from_json_message.user_sender, from_json_message.message_type, from_json_message.message_content]).unwrap();
+                // insert into db 
+                }
+                else if from_json_message.message_type == "get_from_db"{
+                    let last_id: Result<i64> = conn.query_row("SELECT * FROM message ORDER BY id DESC LIMIT 1",[], |row| row.get(0));
+                    match last_id {
+                        Ok(n) =>{
+                            if n < 10 {
+                                let mut sql_prepared = conn.prepare("SELECT * FROM message",).unwrap();
+                                let messages_sql = sql_prepared.query_map([], |row| {
+                                    Ok(Message{
+                                        user_sender: row.get(1)?,
+                                        user_receiver: from_json_message.user_sender.clone(),
+                                        message_type: "set_from_db".to_string(),
+                                        message_content: row.get(3)?, 
+                                    })
+                                }).unwrap();
+                                for message_sql in messages_sql {
+                                    match message_sql {
+                                        Ok(n) =>{
+                                            let json_message = serde_json::to_string(&n).unwrap();
+                                            //println!("{:?}", json_message);
+                                            channel_snd.send(json_message).unwrap();
+
+                                        }
+                                        Err(_) =>{
+                                        }  
+                                    }
+                                }
+                            }
+                            else {
+                                let min = n - 10;
+                                let mut sql_prepared = conn.prepare("SELECT * FROM message WHERE id BETWEEN ? AND ? ",).unwrap();
+                                let messages_sql = sql_prepared.query_map([min, n], |row| {
+                                    Ok(Message{
+                                        user_sender: row.get(1)?,
+                                        user_receiver: from_json_message.user_sender.clone(),
+                                        message_type: "set_from_db".to_string(),
+                                        message_content: row.get(3)?, 
+                                    })
+                                }).unwrap();
+                                for message_sql in messages_sql {
+                                    match message_sql {
+                                        Ok(n) =>{
+                                            let json_message = serde_json::to_string(&n).unwrap();
+                                            //println!("{:?}", json_message);
+                                            channel_snd.send(json_message).unwrap();
+
+                                        }
+                                        Err(_) =>{
+                                        }  
+                                    }
+                                }
+                            }
+                        },
+                        Err(_) =>{}
+                    }
+                }
+            }
+            Err(_) => {
+
+            }
+
         }
+    }
+}
+
+// message type : global
+// message type : login
+// message type : private
+// message type : get_from_db
+// message type : set_from_db
+async fn process (mut user : User, channel_snd : Sender<String>, mut channel_rcv : Receiver<String>, srv_priv_key: RsaPrivateKey, clt_pub_key: RsaPublicKey, mut rng: OsRng) {
+    // data from database
+    let message_back_from_db = Message{
+        user_sender: user.username.clone(),
+        user_receiver: "".to_string(),
+        message_type: "get_from_db".to_string(),
+        message_content: "".to_string(),
+    };
+    let json_message = serde_json::to_string(&message_back_from_db).unwrap();
+    channel_snd.send(json_message).unwrap();
+    loop{
+        match channel_rcv.try_recv() {
+            Ok(mut n) => {
+                trim_newline(&mut user.username);
+                trim_newline(&mut n);
+                let mut from_json_message: Message = serde_json::from_str(&n).unwrap();
+                 
+                if user.username == from_json_message.user_receiver || from_json_message.message_type == "global"{
+                    if from_json_message.message_type == "private" {
+                        let split = from_json_message.message_content.splitn(3, " ");
+                        match split.last() {
+                            Some(value) => {
+                                from_json_message.message_content = value.to_string();
+                            }
+                            None => {
+                            }
+                        }
+                    }
+                    let message_to_send = serde_json::to_string(&from_json_message).unwrap();
+                    let enc_data = clt_pub_key.encrypt(&mut rng, PaddingScheme::new_pkcs1v15_encrypt(), message_to_send.as_bytes()).unwrap();
+                    user.stream.write(&enc_data).await.unwrap();
+                }
+                else if from_json_message.message_type == "login" {
+                    let enc_data = clt_pub_key.encrypt(&mut rng, PaddingScheme::new_pkcs1v15_encrypt(), n.as_bytes()).unwrap();
+                    user.stream.write(&enc_data).await.unwrap();
+                }
+            }
+            Err(_) => {
+            }
+        }
+        let mut data = vec![0; 4096];
+        
+        match user.stream.try_read(&mut data) {
+            Ok(0) => {}
+            Ok(n) => {
+                let dec_data = srv_priv_key.decrypt(PaddingScheme::new_pkcs1v15_encrypt(), &data[..n]).expect("failed to decrypt");
+                assert_ne!(&dec_data, &data[..n]);
+                println!("read {} bytes", n);
+                channel_snd.send(String::from_utf8_lossy(&dec_data).to_string()).unwrap();
+            }
+            Err(_e) => {}
+        }
+    }
+}
+
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let (chann_snd, mut _chann_rcv)  = broadcast::channel(64);
+    let listener = TcpListener::bind("192.168.1.41:1234").await?;
+    // Generate priv and pub key of server
+    let mut rng = OsRng;
+    let bits = 2048;
+    let priv_key = RsaPrivateKey::new(&mut rng, bits).expect("failed to generate a key");
+    let pub_key = RsaPublicKey::from(&priv_key);
+    let pub_key_pem = RsaPublicKey::to_public_key_pem(&pub_key).unwrap();
+    
+
+    let thread_db_send = chann_snd.clone();
+    let thread_db_rcv = chann_snd.subscribe();
+    tokio::spawn(async move {
+        db_process(thread_db_send, thread_db_rcv).await;
+    });
+    println!("Server Initialized");
+    loop {
+        // User accept
+        let (mut socket, addr) = listener.accept().await.unwrap();  
+        socket.readable().await?;
+        
+        // Get client public key
+        let mut client_pkey_buf = [0; 4096];
+        match socket.try_read(&mut client_pkey_buf){
+            Ok(0) => {}
+            Ok(_n) => {}
+            Err(_e) => {}
+        }
+        let mut client_pkey = String::from_utf8_lossy(&client_pkey_buf).to_string();
+        while client_pkey.ends_with('\n') || client_pkey.ends_with('\r') || client_pkey.ends_with('\u{0}') {
+            client_pkey.pop();
+        }
+
+        let json_pkey: Message = serde_json::from_str(&client_pkey).unwrap();
+        println!("{}", json_pkey.message_content);
+        let client_public_key = RsaPublicKey::from_public_key_pem(&json_pkey.message_content).unwrap();
+        
+        // Send Server public key
+        let message_type = "pkey".to_string();
+        let pkey_server_send = Message{
+            user_sender: "".to_string(),
+            user_receiver: "".to_string(),
+            message_type: message_type,
+            message_content: pub_key_pem.to_string(),
+        };
+        let json_message = serde_json::to_string(&pkey_server_send).unwrap();
+        socket.writable().await?;
+        socket.write_all(&json_message.as_bytes()).await.unwrap();
+        
+        // get Client Username
+        let mut buf = [0; 10];
+        let mut buf = ReadBuf::new(&mut buf);
+        poll_fn(|cx|{
+            socket.poll_peek(cx, &mut buf)
+        }).await?;
+
+        let mut username_buf = [0; 4096];
+        
+        socket.readable().await?;
+        let n = socket.read(&mut username_buf[..]).await?;
+        let dec_data = priv_key.decrypt(PaddingScheme::new_pkcs1v15_encrypt(), &username_buf[..n]).expect("failed to decrypt");
+        
+        let mut username_string = String::from_utf8_lossy(&dec_data).to_string();
+        trim_newline(&mut username_string);
+        let mut json_login: Message = serde_json::from_str(&username_string).unwrap();
+        trim_newline(&mut json_login.message_content);
+
+        // User struct
+        let user1 = User{
+            username: json_login.user_sender,
+            stream: socket,
+            _addr: addr,
+        };
+
+          // Thread creation
+        let thread_send = chann_snd.clone();
+        let thread_rcv = chann_snd.subscribe();
+        let priv_key_thread = priv_key.clone();
+        let rng_thread = rng.clone();
+        tokio::spawn(async move {
+            process(user1, thread_send, thread_rcv, priv_key_thread, client_public_key, rng_thread).await;
+        });
+        chann_snd.send(username_string).unwrap();
     }
 }
--- ratz/src/main.rs
+++ ratz/src/main.rs
@@ -1,163 +1,163 @@
-use std::net;
-use std::time;
-use rand::prelude::*;
+use rsa::PublicKey;
+use tokio::net::TcpStream;
+use tokio::io::AsyncReadExt;
+use tokio::io::AsyncWriteExt;
+use tokio::io;
+use tokio::net::tcp::OwnedWriteHalf;
+use std::io::stdin;
+use serde::{Deserialize, Serialize};
+use rsa::{RsaPublicKey, RsaPrivateKey, pkcs8::FromPublicKey, pkcs8::ToPublicKey, PaddingScheme};
+use rand::rngs::OsRng;
 
-fn listen(socket: &net::UdpSocket) -> Vec<u8> {
-    let mut buf: [u8; 20] = [0; 20];
-    let _number_of_bytes: usize = 0;
-    let mut result: Vec<u8> = Vec::new();
-    match socket.recv_from(&mut buf) {
-        Ok((number_of_bytes, _src_addr)) => {
-            println!("received message: {:?}", buf);
-            result = Vec::from(&buf[0..number_of_bytes]);
-        }
-        Err(fail) => println!("failed listening {:?}", fail)
-    }
-    let display_result = result.clone();
-    let result_str = String::from_utf8(display_result).unwrap();
-    println!("received message: {:?}", result_str);
-    result
+#[derive(Serialize, Deserialize)]
+struct Message{
+    user_sender: String,
+    user_receiver: String,
+    message_type: String,
+    message_content: String, 
 }
 
-fn send(socket: &net::UdpSocket, receiver: &str, msg: &Vec<u8>) -> usize {
-
-    println!("sending message: {:?}", msg);
-    let result: usize = 0;
-    match socket.send_to(&msg, receiver) {
-        Ok(number_of_bytes) => println!("{:?}", number_of_bytes),
-        Err(fail) => println!("failed sending {:?}", fail),
-    }
-
-    result
-}
-
-fn init_host(host: &str) -> net::UdpSocket {
-
-    println!("initializing host: {:?}", host);
-    let socket = net::UdpSocket::bind(host).expect("failed to bind host socket");
-    let duration = std::time::Duration::new(1, 0);
-    let dur = std::option::Option::Some(duration);
-    let _res = socket.set_read_timeout(dur).expect("failed to set timeout");
-
-    socket
-}
-
-#[derive(Debug, Default)]
-struct HostConfig {
-    local_ip: String,
-    local_port: String,
-    local_host: String,
-    remote_ip: String,
-    remote_port: String,
-    remote_host: String,
-}
-
-#[derive(Debug)]
-enum CommandInput {
-    local_ip(String),
-    local_port(String),
-    remote_ip(String),
-    remote_port(String),
-    start_host,
-    connect_remote,
-    message(String),
-    unknown(String),
-    error(String),
-}
-
-// fn read_console() -> CommandInput {
-// 
-//     
-//     let mut input = String::with_capacity(25);
-//     match io::stdin().read_line(&mut input) {
-//         Ok(_bytes_read) => {
-//             println!("read: {}", input);
-//             let mut split_input = input.split_whitespace();
-//             let cmd = split_input.next().unwrap();
-//             let data = split_input.collect::<String>();
-//             println!("cmd: {} ------ data: {}", cmd, data);
-//             // identify_comand(&cmd, &data)
-//         }
-//         Err(fail) => {
-//             println!("Failed to read console: {}", fail);
-//             let invalid_data = "failed to read console".to_owned();
-//             CommandInput::error(invalid_data)
-//         }
-//     }
-// }
-
-fn set_host_parameters(ip: &str, port: &str) -> String {
-
-    
-    let mut host = String::with_capacity(128);
-    host.push_str(ip);
-    host.push_str(":");
-    host.push_str(port);
-
-    host
-}
-
-fn build_config(cmd_input: CommandInput, host_config: &mut HostConfig) {
-
-    println!("build: {:?}", cmd_input);
-    match cmd_input {
-        CommandInput::local_ip(ip) => {
-            host_config.local_ip = ip;
-            host_config.local_host = set_host_parameters(&host_config.local_ip,
-                                                         &host_config.local_port);
-        }
-        CommandInput::local_port(port) => {
-            host_config.local_port = port;
-            host_config.local_host = set_host_parameters(&host_config.local_ip,
-                                                         &host_config.local_port);
-        }
-        CommandInput::remote_ip(ip) => {
-            host_config.remote_ip = ip;
-            host_config.remote_host = set_host_parameters(&host_config.remote_ip,
-                                                          &host_config.remote_port);
-        }
-        CommandInput::remote_port(port) => {
-            host_config.remote_port = port;
-            host_config.remote_host = set_host_parameters(&host_config.remote_ip,
-                                                          &host_config.remote_port);
-        }
-        _ => println!("Not a configuration."),
-    }
-
-}
-
-fn main() {
-
-    let mut host_config = HostConfig {
-        local_ip: "192.168.1.41".to_owned(),
-        local_port: "7777".to_owned(),
-        
-        local_host: String::with_capacity(128),
-        remote_ip: "192.168.1.41".to_owned(),
-        remote_port: "53".to_owned(),
-        
-        remote_host: String::with_capacity(128),
+fn trim_newline(s:&mut String){
+    while s.ends_with('\n') || s.ends_with('\r') || s.ends_with('\u{0}') {
+        s.pop();
     };
-    let message = String::from("hello");
+}
 
-    host_config.local_host = set_host_parameters(&host_config.local_ip, &host_config.local_port);
-    host_config.remote_host = set_host_parameters(&host_config.remote_ip, &host_config.remote_port);
+async fn client_input (mut s_write: OwnedWriteHalf, username_string: String, srv_pub_key: RsaPublicKey, mut rng: OsRng) -> OwnedWriteHalf {
+    loop{
+        let mut s=String::new();
+        stdin().read_line(&mut s).expect("Did not enter a correct string");
+       
+        trim_newline(&mut s);
+        // check if message is private or global
+        let chunk: Vec<&str> = s.split(" ").collect();
+        let mut type_message = String::new();
+        let mut receiver_user = String::new();
+        let mut message_must_sended = true;
+        if chunk[0] == "/tell" {
+            if chunk.len() < 2 {
+                message_must_sended = false;
+            }
+            else {
+                type_message = "private".to_string();
+                receiver_user = chunk[1].to_string();
+            }
+        }
+        else {
+            type_message = "global".to_string();
+        }
 
-    let mut socket: net::UdpSocket = init_host(&host_config.local_host);
-    println!("host config: {:?}", host_config);
-    println!("socket: {:?}", socket);
-    let msg_bytes = message.into_bytes();
-    println!("{:?}", msg_bytes);
-
-    
-    let mut sleep_time = std::time::Duration::from_secs(1);
-    let mut rng = rand::thread_rng();
-    std::thread::sleep(sleep_time);
-
-    loop {
-        sleep_time = std::time::Duration::from_secs(rng.gen_range(10..30));
-        std::thread::sleep(sleep_time);
-        send(&socket, &host_config.remote_host, &msg_bytes);
-        let received_msg = listen(&socket);
+        // check if message is correct
+        if message_must_sended {
+            let message_to_send = Message{
+                user_sender: username_string.to_string(),
+                user_receiver: receiver_user,
+                message_type: type_message,
+                message_content: s,
+            };
+        
+            // message sending
+            let json_message = serde_json::to_string(&message_to_send).unwrap();
+            let enc_data = srv_pub_key.encrypt(&mut rng, PaddingScheme::new_pkcs1v15_encrypt(), &json_message.as_bytes()).expect("failed to encrypt");
+            s_write.write_all(&enc_data).await.unwrap();
+        }
     }
+}
+
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    // Generate priv and pub key of client
+    println!("----------------------\nInitialize pub and private key\n----------------------");
+    let mut rng = OsRng;
+    let bits = 2048;
+    let priv_key = RsaPrivateKey::new(&mut rng, bits).expect("failed to generate a key");
+    let pub_key = RsaPublicKey::from(&priv_key);
+    let pub_key_pem = RsaPublicKey::to_public_key_pem(&pub_key).unwrap();
+    println!("----------------------\nPub and Private key was initialize\n----------------------");
+    println!("----------------------\nChoose a username :\n----------------------");    
+    
+    // Username input
+    let mut username = String::new();
+    stdin().read_line(&mut username).expect("Did not enter a correct Username");
+    println!("{:?}", username);
+    trim_newline(&mut username);
+    
+    println!("Your Username is: {}",username);
+    println!("----------------------\nConnect to Server\n----------------------");
+    
+    // TCP Stream creation
+    let mut _stream =  TcpStream::connect("192.168.1.41:1234").await?;
+    let (mut reader, mut writer) = _stream.into_split();
+    println!("----------------------\nConnected to Server\n----------------------");
+    
+    println!("----------------------\nSend Public Key to Server\n----------------------");
+    // Send public key
+    let message_type = "pkey".to_string();
+    let pbkey_to_send = Message{
+        user_sender: "".to_string(),
+        user_receiver: "".to_string(),
+        message_type: message_type,
+        message_content: pub_key_pem,
+    };
+    let json_message = serde_json::to_string(&pbkey_to_send).unwrap();
+    writer.write_all(json_message.as_bytes()).await.unwrap();
+    
+    // Get public key from server
+    let mut buf = [0; 4096];
+    let _readed = reader.read(&mut buf).await;
+    let mut rcv_msg = String::from_utf8_lossy(&buf).to_string();
+    while rcv_msg.ends_with('\n') || rcv_msg.ends_with('\r') || rcv_msg.ends_with('\u{0}') {
+        rcv_msg.pop();
+    };
+
+    let json_message: Message = serde_json::from_str(&rcv_msg).unwrap();
+    println!("{:?}", json_message.message_content);
+    let srv_pub_key = RsaPublicKey::from_public_key_pem(&json_message.message_content).unwrap();
+    
+    println!("----------------------\nSend username to server\n----------------------");
+    // send username to server
+    let message_type = "login".to_string();
+
+    let username_to_send = Message{
+        user_sender: username.to_string(),
+        user_receiver: username.to_string(),
+        message_type: message_type,
+        message_content: username.to_string(),
+    };
+    let json_message = serde_json::to_string(&username_to_send).unwrap();
+    let enc_data = srv_pub_key.encrypt(&mut rng, PaddingScheme::new_pkcs1v15_encrypt(), &json_message.as_bytes()).expect("failed to encrypt");
+    writer.write_all(&enc_data).await.unwrap();
+    println!("----------------------\nConnection initialized\n----------------------");
+    
+    // Spawn thread
+    let rng_thread = rng.clone();
+    tokio::spawn(async move {
+        client_input(writer, username, srv_pub_key, rng_thread).await;
+    });
+    loop {
+        let mut buf = [0; 256];
+        let n = reader.read(&mut buf[..]).await?;
+        let dec_data = priv_key.decrypt(PaddingScheme::new_pkcs1v15_encrypt(), &buf[..n]).expect("failed to decrypt");
+        assert_ne!(&dec_data, &buf[..n]);
+        
+        let mut rcv_msg = String::from_utf8_lossy(&dec_data).to_string();
+        trim_newline(&mut rcv_msg);
+
+        let json_message: Message = serde_json::from_str(&rcv_msg).unwrap();
+        if json_message.message_type == "login" {
+            println!(">> New user logged in : {}",json_message.message_content );    
+            
+        }
+        else if json_message.message_type == "set_from_db" {
+            println!("Previous message of {} :\n{}\n----------", json_message.user_sender, json_message.message_content);
+
+        }
+        else if json_message.message_type == "private" {
+            println!("Private message of {} :\n{}\n----------", json_message.user_sender, json_message.message_content);
+        }
+        else {
+        println!("In {} From {} :\n{}\n----------", json_message.message_type, json_message.user_sender, json_message.message_content);
+        }
+    }   
 }
